f08ae1c3a6e4536a4c20898a72832af1e340a58b
Rafael Nascimento <rafaeln.dev@gmail.com>
2016-03-28 08:51:17 -0300

    [Compose] Removido configuração automatica do gogs.

--- gogs/database_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sed -i "s/NAME =.*$/NAME = ${MYSQL_1_ENV_MYSQL_DATABASE}/" /data/gogs/conf/app.ini
-echo "${MYSQL_1_ENV_MYSQL_DATABASE}"
-sed -i "s/USER =.*$/USER = ${MYSQL_1_ENV_MYSQL_USERNAME}/" /data/gogs/conf/app.ini
-echo "${MYSQL_1_ENV_MYSQL_USERNAME}"
-sed -i "s/PASSWD =.*$/PASSWD = ${MYSQL_1_ENV_MYSQL_ROOT_PASSWORD}/" /data/gogs/conf/app.ini
-echo "${MYSQL_1_ENV_MYSQL_ROOT_PASSWORD}"
-
