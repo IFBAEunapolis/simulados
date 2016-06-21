73a61971e7211f9f6331ff2acd03aff3dfc8d7e6
Rafael Nascimento <rafaeln.dev@gmail.com>
2016-06-21 15:05:32 -0300

    [Outro] Script para compilar a documentação (dev)

--- build_docs.sh
+++ build_docs.sh
@@ -3,7 +3,9 @@
 #   gitbook build
 # done
 
-inotifywait -rm "./" --format '%w%f' --exclude '(_book|db-data|.git|gogs|jenkins|node_modules)' -e close_write -e create -e delete -e move |
-    while read file; do
-      gitbook build
-    done
+inotifywait -rm "./" --format '%w%f' --exclude '(_book|db-data|.git|gogs|jenkins|node_modules)' -e close_write -e create -e delete -e move | while read file ; do
+  gitbook build
+done &
+
+live-server _book
+
